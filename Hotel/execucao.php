f69d5eef87dbb8ab9566a421035444814d922dfe
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2024-11-14 15:40:58 -0300

    Create execucao.php

---
+++ Hotel/execucao.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once ("modelo/Quarto.php");
+require_once ("modelo/SistemaHotel.php");
+require_once ("modelo/Suite.php");
+require_once ("modelo/Luxo.php");
+
+class execucao{
+
+    
+}
