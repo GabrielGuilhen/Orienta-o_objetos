4d8428c827158645b703d2963632e3d82b8837ca
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2024-11-14 15:39:56 -0300

    Create Suite.php

---
+++ Hotel/modelo/Suite.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once ("modelo/Quarto.php");
+require_once("modelo/Luxo.php");
+require_once("modelo/SistemaHotel.php");
+require_once("execucao.php");
+
+class Suite extends Quarto{
+
+    
+
+}
