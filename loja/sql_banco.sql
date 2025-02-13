6ee59582de0acb9b12b480c44b1a7fcf84a45d57
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-02-13 13:25:22 -0300

    Create sql_banco.sql

---
+++ loja/sql_banco.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE loja;
+USE loja;
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidade INT NOT NULL,
+    categoria VARCHAR(50)
+);
