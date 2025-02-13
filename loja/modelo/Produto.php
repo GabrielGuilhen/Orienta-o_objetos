f49786f6a45fd0e2d00b0c7e3ac16b9bd27533da
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-02-13 15:24:33 -0300

    Update Produto.php

--- loja/modelo/Produto.php
+++ loja/modelo/Produto.php
@@ -1,5 +1,6 @@
 <?php
-class Produto {
+class Produto
+{
     private $id;
     private $nome;
     private $descricao;
@@ -8,7 +9,9 @@ class Produto {
     private $categoria;
 
     // Construtor
-    public function __construct($nome, $descricao, $preco, $quantidade, $categoria) {
+    public function __construct($nome, $descricao, $preco, $quantidade, $categoria, $id = null)
+    {
+        $this->id = $id; // id pode ser opcional, caso não seja informado.
         $this->nome = $nome;
         $this->descricao = $descricao;
         $this->preco = $preco;
@@ -16,53 +19,75 @@ class Produto {
         $this->categoria = $categoria;
     }
 
-    // Getters e Setters
-    public function getId() {
+    // Getter e Setter para ID
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function setId($id) {
+    public function setId($id)
+    {
         $this->id = $id;
     }
 
-    public function getNome() {
+    // Getter e Setter para Nome
+    public function getNome()
+    {
         return $this->nome;
     }
 
-    public function setNome($nome) {
+    public function setNome($nome)
+    {
         $this->nome = $nome;
     }
 
-    public function getDescricao() {
+    // Getter e Setter para Descrição
+    public function getDescricao()
+    {
         return $this->descricao;
     }
 
-    public function setDescricao($descricao) {
+    public function setDescricao($descricao)
+    {
         $this->descricao = $descricao;
     }
 
-    public function getPreco() {
+    // Getter e Setter para Preço
+    public function getPreco()
+    {
         return $this->preco;
     }
 
-    public function setPreco($preco) {
+    public function setPreco($preco)
+    {
         $this->preco = $preco;
     }
 
-    public function getQuantidade() {
+    // Getter e Setter para Quantidade
+    public function getQuantidade()
+    {
         return $this->quantidade;
     }
 
-    public function setQuantidade($quantidade) {
+    public function setQuantidade($quantidade)
+    {
         $this->quantidade = $quantidade;
     }
 
-    public function getCategoria() {
+    // Getter e Setter para Categoria
+    public function getCategoria()
+    {
         return $this->categoria;
     }
 
-    public function setCategoria($categoria) {
+    public function setCategoria($categoria)
+    {
         $this->categoria = $categoria;
     }
+
+    // Método para exibir o produto como string
+    public function __toString()
+    {
+        return "Produto: {$this->nome}\nDescrição: {$this->descricao}\nPreço: {$this->preco}\nQuantidade: {$this->quantidade}\nCategoria: {$this->categoria}";
+    }
 }
-?>
