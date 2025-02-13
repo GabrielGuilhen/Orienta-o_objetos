21cefff22b2ddf91becbb3ae3bf30ae5b9b9e007
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-02-13 13:31:11 -0300

    Create conexao.php

---
+++ loja/util/conexao.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conexao {
+
+    private static $con=null;
+
+    public static function getCon() {
+
+        if(self::$con==null){
+
+            $opcoes=array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8",
+                         PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC);
+
+            $dadosCon="mysql:host=localhost:3306;dbname=produtos";
+            self::$con=new PDO($dadosCon, "root", "root", $opcoes);
+            
+        }
+
+        return self::$con;
+        
+    }
+
+}
