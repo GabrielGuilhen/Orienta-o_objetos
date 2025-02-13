1ae6e8c48127a2834898ed4de7b72d9e93228b55
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-02-13 15:23:43 -0300

    Update execucao.php

--- loja/execucao.php
+++ loja/execucao.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'classes/ProdutoDAO.php';
+
+require_once("modelo/Produto.php");
+require_once("dao/ProdutoDAO.php");
 
 $dao = new ProdutoDAO();
 
@@ -13,18 +15,73 @@ $opcao = readline();
 
 switch ($opcao) {
     case 1:
-        require 'inserir.php';
+        echo "Nome: ";
+        $nome = readline();
+        echo "Descrição: ";
+        $descricao = readline();
+        echo "Preço: ";
+        $preco = readline();
+        echo "Quantidade: ";
+        $quantidade = readline();
+        echo "Categoria: ";
+        $categoria = readline();
+
+        $produto = new Produto($nome, $descricao, $preco, $quantidade, $categoria);
+        $dao->inserir($produto);
         break;
+
     case 2:
-        require 'listar.php';
+        $produtoDAO = new ProdutoDAO();
+        $produtos = $produtoDAO->listar();
+
+        echo "---------------------------------\n";
+        foreach ($produtos as $produto) {
+            echo "ID: " . $produto->getId() . "\n";
+            echo "Nome: " . $produto->getNome() . "\n";
+            echo "Descrição: " . $produto->getDescricao() . "\n";
+            echo "Preço: " . $produto->getPreco() . "\n";
+            echo "Quantidade: " . $produto->getQuantidade() . "\n";
+            echo "Categoria: " . $produto->getCategoria() . "\n";
+            echo "---------------------------------\n";
+        }
         break;
+
     case 3:
-        require 'buscar.php';
+        $id = readline("Informe o ID do Produto: ");
+        $produtoDAO = new ProdutoDAO();
+        $produto = $produtoDAO->buscarPorId($id);
+
+        if ($produto != null) {
+            echo "ID: " . $produto->getId() . "\n";
+            echo "Nome: " . $produto->getNome() . "\n";
+            echo "Descrição: " . $produto->getDescricao() . "\n";
+            echo "Preço: " . $produto->getPreco() . "\n";
+            echo "Quantidade: " . $produto->getQuantidade() . "\n";
+            echo "Categoria: " . $produto->getCategoria() . "\n";
+        } else {
+            echo "Produto não encontrado!\n";
+        }
         break;
+
     case 4:
-        require 'excluir.php';
+        $produtoDAO = new ProdutoDAO();
+        $produtos = $produtoDAO->listar();
+
+        echo "---------------------------------\n";
+        foreach ($produtos as $produto) {
+            echo "ID: " . $produto->getId() . "\n";
+            echo "Nome: " . $produto->getNome() . "\n";
+            echo "Descrição: " . $produto->getDescricao() . "\n";
+            echo "Preço: " . $produto->getPreco() . "\n";
+            echo "Quantidade: " . $produto->getQuantidade() . "\n";
+            echo "Categoria: " . $produto->getCategoria() . "\n";
+            echo "---------------------------------\n";
+        }
+
+        $id = readline("Informe o ID do produto a ser excluído: ");
+        $produtoDAO->excluir($id);
         break;
+
     default:
         echo "Opção inválida!\n";
 }
-?>
